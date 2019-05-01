1c8d8d00c1824a055c200f8a1172bf3bbde71734
Ken Mueller <kenmueller0@gmail.com>
2019-05-01 00:22:09 -0700

    Cleaned solutions

--- exercises/hard/08_safeGet.test.js
+++ exercises/hard/08_safeGet.test.js
@@ -6,28 +6,19 @@
  * it returns null.
 */
 
-
 function safeGet(obj, path) {
-  const keys = path.split('.')
-  return keys.reduce((acc, key) => {
-    return (acc || {})[key] ? acc[key] : null 
-  }, obj)
+	const keys = path.split('.')
+	return keys.reduce((acc, key) => (acc || {})[key] ? acc[key] : null, obj)
 }
 
-/* =========== DON'T CHANGE THE CODE AFTER THIS LINE ===============" */
+/* =========== DON'T CHANGE THE CODE AFTER THIS LINE =============== */
 
-const obj = {
-  a: {
-    b: {
-      c: 42
-    }
-  }
-}
+const obj = { a: { b: { c: 42 } } }
 
-test('gets a value by path', () => {
-  expect(safeGet(obj, 'a.b.c')).toEqual(42)
-})
+test('gets a value by path', () =>
+	expect(safeGet(obj, 'a.b.c')).toEqual(42)
+)
 
-test('returns null for non-existing values', () => {
-  expect(safeGet(obj, 'a.b.c.d')).toEqual(null)
-})
+test('returns null for non-existing values', () =>
+	expect(safeGet(obj, 'a.b.c.d')).toEqual(null)
+)
--- exercises/medium/05_reduce.test.js
+++ exercises/medium/05_reduce.test.js
@@ -4,20 +4,16 @@
  * You can implement almost anything with reduce.
  * But how do you implement reduce?
  * TIP: Use your friend, recursion
- *
 */
 
 function reduce(arr, fn, initial) {
-  if (arr.length === 0) return initial
-  const [head, ...tail] = arr
-  return reduce(tail, fn, fn(initial, head))
+	if (arr.length === 0) return initial
+	const [head, ...tail] = arr
+	return reduce(tail, fn, fn(initial, head))
 }
 
-/* =========== DON'T CHANGE THE CODE AFTER THIS LINE ===============" */
+/* =========== DON'T CHANGE THE CODE AFTER THIS LINE =============== */
 
-test('reduces an array', () => {
-  const sum = (a, b) => a + b
-  expect(
-    reduce([1, 2, 3, 4, 5], sum, 0)
-  ).toEqual(15)
-})
+test('reduces an array', () =>
+	expect(reduce([1, 2, 3, 4, 5], (a, b) => a + b, 0)).toEqual(15)
+)
